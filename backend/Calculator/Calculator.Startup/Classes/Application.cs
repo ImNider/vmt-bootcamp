04b67ca2e878f88d1444ba8f716812e372b4e2d5
Neider Vélez <velezzambranoneider@gmail.com>
2026-03-18 20:27:18 -0500

    Backend: Calculadora básica completada

--- backend/Calculator/Calculator.Startup/Classes/Application.cs
+++ backend/Calculator/Calculator.Startup/Classes/Application.cs
@@ -1,6 +1,5 @@
 ﻿using Calculator.Startup.Models;
 using Calculator.Startup.Interfaces;
-using System.Reflection.Metadata.Ecma335;
 
 namespace Calculator.Startup.Classes
 {
@@ -18,21 +17,21 @@ namespace Calculator.Startup.Classes
             });
             _options.Add(new Option()
             {
-                Id = 1,
+                Id = 2,
                 DisplayName = "Restar dos números",
                 Usage = "<numero> <numero>",
                 Return = "Resultado: <numero>"
             });
             _options.Add(new Option()
             {
-                Id = 1,
+                Id = 3,
                 DisplayName = "Multiplicar dos números",
                 Usage = "<numero> <numero>",
                 Return = "Resultado: <numero>"
             });
             _options.Add(new Option()
             {
-                Id = 1,
+                Id = 4,
                 DisplayName = "Dividir dos números",
                 Usage = "<numero> <numero>",
                 Return = "Resultado: <numero>"
@@ -60,11 +59,11 @@ namespace Calculator.Startup.Classes
                 ---------------------------------
 
                 Opciones:
-                {string.Join("\n", _options.Select((cmd) => $"{cmd.Id}. {cmd.DisplayName}\n{cmd.Usage}\n{cmd.Return}"))}
+                {string.Join("\n", _options.Select((op) => $"{op.Id}. {op.DisplayName}\n{op.Usage}\n{op.Return}"))}
 
                 ---------------------------------
                 """;
-            Console.WriteLine(message);
+            ShowMessage(message, ConsoleColor.Green);
         }
         public double Sum(double number1, double number2)
         {
@@ -80,6 +79,7 @@ namespace Calculator.Startup.Classes
         }
         public double Divide(double number1, double number2)
         {
+            if(number2==0) throw new DivideByZeroException();
             return number1 / number2;
         }
         public void Start()
@@ -91,13 +91,36 @@ namespace Calculator.Startup.Classes
                 try
                 {
                     var optionID = Convert.ToInt32(ShowQuestion("Seleccione una opción: ", ConsoleColor.Blue));
+
+                    var findOption = _options.Where((op) => op.Id == optionID).FirstOrDefault();
+
+                    if (findOption is null)
+                    {
+                        throw new Exception("Error: Comando no encontrado");
+                    }
+
                     var number1 = Convert.ToDouble(ShowQuestion("Numero 1: ", ConsoleColor.Yellow));
                     var number2 = Convert.ToDouble(ShowQuestion("Numero 2: ", ConsoleColor.Yellow));
-                }
-                catch (Exception)
-                {
 
-                    throw;
+                    if (findOption.Id == optionID)
+                    {
+                        if (optionID == 1) Console.WriteLine($"Suma: {Sum(number1, number2)}");
+                        if (optionID == 2) Console.WriteLine($"Resta: {Subtract(number1, number2)}");
+                        if (optionID == 3) Console.WriteLine($"Multiplicacion: {Multiply(number1, number2)}");
+                        if (optionID == 4) Console.WriteLine($"Division: {Divide(number1, number2)}");
+                    }
+                }
+                catch (FormatException)
+                {
+                    ShowMessage("Error: Entrada no válida. Por favor, ingrese un número.", ConsoleColor.Red);
+                }   
+                catch(DivideByZeroException)
+                {
+                    ShowMessage("Error: No se puede dividir por cero.", ConsoleColor.Red);
+                }
+                catch (Exception ex)
+                {
+                    ShowMessage(ex.Message, ConsoleColor.Red);
                 }
             }
         }
