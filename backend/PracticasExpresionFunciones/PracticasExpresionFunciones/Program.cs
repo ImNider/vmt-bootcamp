71f5a55e4db2be763d3388ae1d7d63f367c363d5
Neider Vélez <velezzambranoneider@gmail.com>
2026-04-17 12:33:15 -0500

    feat: agregar practica de expresiones

---
+++ backend/PracticasExpresionFunciones/PracticasExpresionFunciones/Program.cs
@@ -0,0 +1,20 @@
+﻿// Definir una función que puedan llamar: Tipos de datos simples
+using System.Linq.Expressions;
+
+Func<int, int, int> Sumar = (num1, num2) =>
+{
+    return num1 + num2;
+};
+
+// Definir una expresión: Tanto con funciones que usen un tipo o un modelo (class)
+Expression<Func<int, bool>> expresion = x => x > 10;
+Expression<Func<Usuario, bool> validarEdadUsuario = x => x.edad >= 18;
+
+// Un modelo (class), con el que usen su función, para practicar el uso de propiedades
+public class Usuario()
+{
+    string Nombre { get; set; } = null!;
+    int edad { get; set; }
+}
+
+// Creen un repositorio, donde se manejen un genérico y hagan uso de expresiones. Colocar constraint para unicamente clases
